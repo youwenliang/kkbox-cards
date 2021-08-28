44550cdaa5f15db32a63b99c650ab7a2adaa345b
youwenliang <youwen.mark.liang@gmail.com>
2021-08-29 00:04:42 +0800

    update search

--- src/App.js
+++ src/App.js
@@ -8,16 +8,39 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
+      keyword: '',
+      type: 'artist'
     };
+    this.onValueChange = this.onValueChange.bind(this);
+    this.getInputValue = this.getInputValue.bind(this);
+    this.getInfo = this.getInfo.bind(this);
   }
 
   componentDidMount() {
-    fetch('https://api.kkbox.com/v1.1/search?q=Mayday&type=track&territory=TW&offset=0&limit=50', {
+    
+  }
+
+  onValueChange(event) {
+    this.setState({
+      type: event.target.value
+    });
+  }
+
+  getInputValue(event) {
+    event.preventDefault();
+    var keyword = document.getElementById("input").value;
+    var type = document.querySelector('input[name="type"]:checked').value;
+    this.setState({keyword: keyword, type: type});
+    this.getInfo(keyword, type);
+  }
+
+  getInfo(keyword, type) {
+    fetch('https://api.kkbox.com/v1.1/search?q='+keyword+'&type='+type+'&territory=TW&offset=0&limit=10', {
       method: 'GET',
       headers: {
         Accept: 'application/json',
-        authorization: 'Bearer F5MKSIpHAt9ukR7tWo9vdg==',
+        //authorization: 'Bearer F5MKSIpHAt9ukR7tWo9vdg==!',
       },
     })
       .then(res => res.json())
@@ -25,8 +48,9 @@ class App extends Component {
         (result) => {
           this.setState({
             isLoaded: true,
-            items: result.tracks.data
+            items: result[type+'s'].data
           });
+          console.log(result)
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -42,21 +66,39 @@ class App extends Component {
 
   render() {
     const { error, isLoaded, items } = this.state;
+    let result = [];
+
     if (error) {
-      return <div>Error: {error.message}</div>;
+      result = (<div>Error: {error.message}</div>);
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      result = (<div>Loading...</div>);
     } else {
-      return (
+      result = (
         <ul>
           {items.map(item => (
             <li key={item.id}>
-              {item.name} {item.url}
+              <span>{item.name} {item.url}</span>
+              <img src={(item.images === undefined) ? item.album.images[0].url : item.images[0].url} width="100" />
             </li>
           ))}
         </ul>
-      );
+      )
     }
+    return (
+      <main>
+        <form action="javascript:void(0);">
+          <input id="input" type="text" placeholder="輸入歌手名稱"/>
+            <input type="radio" id="artist" name="type" value="artist" onChange={this.onValueChange} checked={this.state.type === "artist"}/>
+            <label>歌手</label>
+            <input type="radio" id="album" name="type" value="album" onChange={this.onValueChange} checked={this.state.type === "album"}/>
+            <label>專輯</label>
+            <input type="radio" id="track" name="type" value="track" onChange={this.onValueChange} checked={this.state.type === "track"}/>
+            <label className="mr2">歌曲</label>
+          <input type="submit" value="搜尋" onClick={this.getInputValue}/>
+        </form>
+        {result}
+      </main>
+    )
   }
 }
 export default App;
